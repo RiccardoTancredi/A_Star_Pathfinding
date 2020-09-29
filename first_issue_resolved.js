0b25d6fe617910ab4699e3afda48026b1c2ff61c
RiccardoTancredi <ric.tancredi@gmail.com>
2020-09-29 15:02:46 +0200

    In first_issue_resolved there is the solution to the first issue merged
    The program is incomplete. Now it shows all the possible patters even though sometimes there are some bugs.
    It has to show only the best pattern, not all it finds

--- first_issue_resolved.js
+++ first_issue_resolved.js
@@ -15,7 +15,7 @@ function black_or_white(grid, cols) {
       if (grid[i][j] == 1) {
         number += 1;
       }
-      if (number == floor(cols / 5)) {
+      if (number == floor(cols / 2)) {
         for (let k = i; k < grid[j].length; k++) {
           grid[k][j] = 0;
         }
@@ -50,33 +50,27 @@ function g_cost(start, x, y) {
   return distance_from_start;
 }
 
-function a_star(grid, start, end, pattern_of_patterns) {
+function a_star(grid, start, end, collection_of_grids) {
   try {
-    console.log("I'm here in the first try");
     if (start[0] == end[0] && start[1] == end[1]) {
       grid[end[0]][end[1]] = 3;
       console.log("A pattern has been found!");
-      pattern_of_patterns.push(grid);
+      var temp_grid = grid;
+      collection_of_grids.push(temp_grid);
       for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
           if (grid[i][j] == 2) {
             start = [i, j];
           }
-          // Here there is something missing
-          /*
-          The idea in all of this part is to collect all the possible patterns in an array called pattern_of_patterns. Then we will select the pattern with the least
-          number of green cells, represented with grid[i][j] = 4. Now I have to find a better way to do the thing a the bottom here, because I don't want tp change 
-          the value grid[i][j] to 5, because even if a cell has become green in a pattern it could steel be green in another one.
-          I can not say grid[i][j] = 4 because this would lead to an infinte loop, because the programm would always analyze the same pattern over and over again.
-          
-                                                                              #ToDo
-          */
           if (grid[i][j] == 4) {
             grid[i][j] = 5; // change green to other in order to not be analyzed again later
           }
         }
       }
-      return a_star(grid, start, end, pattern_of_patterns);
+      // console.log(grid);
+      // console.log(collection_of_grids);
+      return a_star(grid, start, end, collection_of_grids);
+      // return grid;
     }
     // console.log("Lo start è", start);
     // console.log("L'end è", end);
@@ -87,12 +81,12 @@ function a_star(grid, start, end, pattern_of_patterns) {
       i++
     ) {
       /* 
-        Here next_grid is a 2D array in which are stored in the first array the value of the f_cost function (f = g+h), and the second array 
-        a tuple which contains the coordinates of the cells evalueted.
-        The goal here is to store all of these values, then find the shortest f_cost value so move to that cell
-        #ToDo: what to do if there are cells with the same f_cost?
-        Then the new cell will be the new start and it will loop again until the cell becomes the end cell then noLoop()
-      */
+          Here next_grid is a 2D array in which are stored in the first array the value of the f_cost function (f = g+h), and the second array 
+          a tuple which contains the coordinates of the cells evalueted.
+          The goal here is to store all of these values, then find the shortest f_cost value so move to that cell
+          #ToDo: what to do if there are cells with the same f_cost?
+          Then the new cell will be the new start and it will loop again until the cell becomes the end cell then noLoop()
+          */
       for (
         let j = Math.abs(start[1] - 1) % (start[1] + 1);
         j <= start[1] + ((start[1] + 1) % grid.length) / (start[1] + 1);
@@ -116,21 +110,17 @@ function a_star(grid, start, end, pattern_of_patterns) {
     // console.log("L'indice del minimo vale", temp_index);
     index_of_new_start = next_grid[1][temp_index];
     start = index_of_new_start;
-    grid[start[0]][start[1]] = 4; // green cell
-    // if (pattern_of_patterns.length == 0) {
-    //   pattern_of_patterns = [];
-    // }
-    return a_star(grid, start, end, pattern_of_patterns);
+    grid[start[0]][start[1]] = 4;
+    return a_star(grid, start, end, collection_of_grids);
   } catch {
     console.log("I'm here in catch");
-    try {
-      console.log("I'm here in the second try");
+    if (collection_of_grids.length >= 1) {
       let patterns = [];
-      for (let k = 0; k < pattern_of_patterns.length; k++) {
+      for (let k = 0; k < collection_of_grids.length; k++) {
         let number_of_green_cells = 0;
         for (let i = 0; i < rows; i++) {
           for (let j = 0; j < cols; j++) {
-            if (pattern_of_patterns[k][i][j] == 4) {
+            if (collection_of_grids[k][i][j] == 4) {
               number_of_green_cells += 1;
             }
           }
@@ -139,22 +129,27 @@ function a_star(grid, start, end, pattern_of_patterns) {
       }
       console.log(patterns);
       let least_number_of_green_cells = Math.min.apply(null, patterns);
-      grid = pattern_of_patterns[patterns.indexOf(least_number_of_green_cells)]; //final grid
-      // here I have to make green again the cells that where green
-      // there's a loop that I have to stop somehow
-
-      // for (let i = 0; i < rows; i++) {
-      //   for (let j = 0; j < cols; j++) {
-      //     if (grid[i][j] == 5) {          // I think I don't need this because the grids added in pattern_of_patterns are already good for being drawn
-      //       grid[i][j] = 4;
-      //     }
-      //   }
-      // }
-      console.log(pattern_of_patterns);
-
+      grid = collection_of_grids[patterns.indexOf(least_number_of_green_cells)]; //final grid
+      console.log(collection_of_grids);
+      for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+          if (grid[i][j] == 4) {
+            grid[i][j] = 0;
+          }
+          if (grid[i][j] == 5) {
+            grid[i][j] = 4; 
+          }
+        }
+      }
+      console.log("DONE!");
       return grid;
-    } catch {}
-    throw "There is no pattern available; Reload and try again!";
+    }
+  }
+  if (collection_of_grids.length === 0) {
+    throw "There is No Pattern Available";
+  }
+  else {
+    throw "The Best Pattern Has Been Found";
   }
 }
 
@@ -163,8 +158,6 @@ function setup() {
   cols = width / resolution;
   rows = height / resolution;
   grid = black_or_white(make2DArray(rows, cols), cols);
-  console.log("Let's start");
-  console.log(grid);
 }
 
 function draw() {
@@ -196,7 +189,7 @@ function draw() {
   }
   if (start != 0 && end != 0 && start_button != 0) {
     grid = a_star(grid, start, end, []);
-    start_button = 10;
+    start_button = 0;
   }
 }
 
